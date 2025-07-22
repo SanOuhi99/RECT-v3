426c11e7fbbf8a15c3ab6e55f56224d43e8a8753
SanOuhi99 <sanouhi99@gmail.com>
2025-07-22 19:18:06 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, EmailStr
 import csv, uuid, os, datetime, json
 import pathlib, os
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.add_middleware(

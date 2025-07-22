303fd046c0a42da24adfc3f0b0fac4b171224ea9
SanOuhi99 <sanouhi99@gmail.com>
2025-07-22 11:30:53 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route , Navigate} from 'react-router-dom';
 import RECTLandingPage from './RECTLandingPage';
 import AgentLogin from './pages/AgentLogin';
 import AgentSignup from './pages/AgentSignup';
@@ -8,6 +8,7 @@ import AdminDashboard from './pages/AdminDashboard';
 const App = () => {
   return (
     <Routes>
+      <Route path="*" element={<Navigate to="/" replace />} />
       <Route path="/" element={<RECTLandingPage />} />
       <Route path="/agent-login" element={<AgentLogin />} />
       <Route path="/agent-signup" element={<AgentSignup />} />
